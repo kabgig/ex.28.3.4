9b19a243e510540c75b8282b3d83865376f5da68
Ansar Ismagilov <ansar89kab@bk.ru>
2023-07-21 20:22:54 +0800

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws NoSuchFieldException {
+        AnnotationExample map = new AnnotationExample();
+        Author annotation = map.getClass().getAnnotation(Author.class);
+        System.out.println("Author: " + annotation.value());
+    }
+
+    @Target(ElementType.TYPE)
+    @Retention(RetentionPolicy.RUNTIME)
+    public @interface Author {
+        String value();
+    }
+
+    @Author("Stephen King")
+    public static class AnnotationExample {
     }
 }
